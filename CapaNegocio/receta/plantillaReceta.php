4999a1c19ce00f436ba45d6c643c2aff9b7b737c
dayler99 <daylertaboadafrias@gmail.com>
2024-07-08 22:56:18 -0400

    cuarto

--- CapaNegocio/receta/plantillaReceta.php
+++ CapaNegocio/receta/plantillaReceta.php
@@ -1,11 +1,11 @@
 <?php
-require('../../fpdf/fpdf.php');
+require('https://tallerg-production.up.railway.app/fpdf/fpdf.php');
 
     class PDF extends FPDF{
         function Header(){
             setlocale(LC_ALL,"es_ES@euro","es_ES","esp");
       
-            $this->Image('../../Public/Imagen/logotipo.png',5,5,80);
+            $this->Image('https://tallerg-production.up.railway.app/Public/Imagen/logotipo.png',5,5,80);
             $this->SetFont('Arial','B',22);
             $this->Cell(80);
             $this->Cell(130,35,'RECETA',0,1,'C');
